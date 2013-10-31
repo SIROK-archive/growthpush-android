c3227c78b80f2b7322cb812cad81aead3044e45a
Naoyuki Kataoka <kataokanaoyuki@gmail.com>
2013-10-31 16:42:28 +0900

    Add DefaultReceiveHandler.Callback and Make VIBRATE and WAKE_LOCK permission optional

--- src/com/growthpush/handler/DefaultReceiveHandler.java
+++ src/com/growthpush/handler/DefaultReceiveHandler.java
@@ -11,6 +11,7 @@ import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
 
+import com.growthpush.utils.PermissionUtils;
 import com.growthpush.view.AlertActivity;
 
 /**
@@ -20,6 +21,17 @@ import com.growthpush.view.AlertActivity;
  * 
  */
 public class DefaultReceiveHandler implements ReceiveHandler {
+
+	private Callback callback = new Callback();
+
+	public DefaultReceiveHandler() {
+		super();
+	}
+
+	public DefaultReceiveHandler(DefaultReceiveHandler.Callback callback) {
+		this();
+		setCallback(callback);
+	}
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
@@ -35,8 +47,11 @@ public class DefaultReceiveHandler implements ReceiveHandler {
 			return;
 
 		Intent alertIntent = new Intent(context, AlertActivity.class);
-		alertIntent.putExtra("message", intent.getExtras().getString("message"));
+		alertIntent.putExtras(intent.getExtras());
+		alertIntent.putExtra("showDialog", true);
 		alertIntent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_NO_HISTORY | Intent.FLAG_ACTIVITY_NEW_TASK);
+
+		AlertActivity.setSharedCallback(callback);
 		context.startActivity(alertIntent);
 
 	}
@@ -66,22 +81,44 @@ public class DefaultReceiveHandler implements ReceiveHandler {
 		String message = extras.getString("message");
 		boolean sound = extras.getBoolean("sound", false);
 
-		PendingIntent pendingIntent = PendingIntent.getActivity(context, 0,
-				packageManager.getLaunchIntentForPackage(context.getPackageName()).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK),
-				PendingIntent.FLAG_CANCEL_CURRENT);
+		Intent intent = new Intent(context, AlertActivity.class);
+		intent.putExtra("showDialog", false);
+		intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 
-		NotificationCompat.Builder builder = new NotificationCompat.Builder(context).setTicker(title).setSmallIcon(icon)
-				.setContentTitle(title).setContentText(message).setContentIntent(pendingIntent).setWhen(System.currentTimeMillis())
-				.setAutoCancel(true);
-		if (sound) {
-			builder.setDefaults(Notification.DEFAULT_SOUND);
-			try {
-				builder.setDefaults(Notification.DEFAULT_VIBRATE | Notification.DEFAULT_LIGHTS);
-			} catch (SecurityException e) {
-			}
-		}
+		AlertActivity.setSharedCallback(callback);
+		PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+
+		NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
+		builder.setTicker(title);
+		builder.setSmallIcon(icon);
+		builder.setContentTitle(title);
+		builder.setContentText(message);
+		builder.setContentIntent(pendingIntent);
+		builder.setWhen(System.currentTimeMillis());
+		builder.setAutoCancel(true);
+
+		if (sound && PermissionUtils.permitted(context, "android.permission.VIBRATE"))
+			builder.setDefaults(Notification.DEFAULT_SOUND | Notification.DEFAULT_VIBRATE | Notification.DEFAULT_LIGHTS);
 
 		return builder.build();
 
 	}
+
+	public Callback getCallback() {
+		return callback;
+	}
+
+	public void setCallback(Callback callback) {
+		this.callback = callback;
+	}
+
+	public static class Callback {
+
+		public void onOpen(Context context, Intent intent) {
+			context.startActivity(context.getPackageManager().getLaunchIntentForPackage(context.getPackageName())
+					.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+		}
+
+	}
+
 }
