793782202ded88cbdd90a8c78894494a2ff17222
Naoyuki Kataoka <kataokanaoyuki@gmail.com>
2013-11-01 12:04:43 +0900

    Add constructor with Callback argument to OnlyAlertReceiveHandler and OnlyNotificationReceiveHandler

--- src/com/growthpush/handler/OnlyAlertReceiveHandler.java
+++ src/com/growthpush/handler/OnlyAlertReceiveHandler.java
@@ -5,6 +5,15 @@ import android.content.Intent;
 
 public class OnlyAlertReceiveHandler extends DefaultReceiveHandler {
 
+	public OnlyAlertReceiveHandler() {
+		super();
+	}
+
+	public OnlyAlertReceiveHandler(DefaultReceiveHandler.Callback callback) {
+		this();
+		setCallback(callback);
+	}
+
 	@Override
 	public void onReceive(Context context, Intent intent) {
 
--- src/com/growthpush/handler/OnlyNotificationReceiveHandler.java
+++ src/com/growthpush/handler/OnlyNotificationReceiveHandler.java
@@ -5,6 +5,15 @@ import android.content.Intent;
 
 public class OnlyNotificationReceiveHandler extends DefaultReceiveHandler {
 
+	public OnlyNotificationReceiveHandler() {
+		super();
+	}
+
+	public OnlyNotificationReceiveHandler(DefaultReceiveHandler.Callback callback) {
+		this();
+		setCallback(callback);
+	}
+
 	@Override
 	public void onReceive(Context context, Intent intent) {
 
