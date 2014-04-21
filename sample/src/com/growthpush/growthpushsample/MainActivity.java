2c1cb3ef1539174543affcb3f23e0479ac7751fd
Naoyuki Kataoka <kataokanaoyuki@gmail.com>
2014-04-21 20:02:05 +0900

    Add Growth Push initialization code to sample project

--- sample/src/com/growthpush/growthpushsample/MainActivity.java
+++ sample/src/com/growthpush/growthpushsample/MainActivity.java
@@ -1,8 +1,11 @@
 package com.growthpush.growthpushsample;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+
+import com.growthpush.GrowthPush;
+import com.growthpush.model.Environment;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +13,14 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		GrowthPush
+				.getInstance()
+				.initialize(getApplicationContext(), 1071, "Ou3DgCwmMS2tBocWXGKSnRUUTyVA078n",
+						BuildConfig.DEBUG ? Environment.development : Environment.production, true).register("955057365401");
+		GrowthPush.getInstance().trackEvent("Launch");
+		GrowthPush.getInstance().setDeviceTags();
+
 	}
 
 	@Override
