b53821a19650b97bfe4f75fac2051806add9a13e
Shigeru Ogawa <ogawa@sirok.co.jp>
2014-11-17 21:22:24 +0900

    Remove request method from Model class.

--- source/src/com/growthpush/model/Model.java
+++ source/src/com/growthpush/model/Model.java
@@ -1,19 +1,6 @@
 package com.growthpush.model;
 
-import java.util.Map;
-
-import org.json.JSONObject;
-
-import com.growthpush.GrowthPush;
 
 public class Model {
-
-	public JSONObject post(final String api, Map<String, Object> params) {
-		return GrowthPush.getInstance().getHttpClient().post("1/" + api, params);
-	}
-
-	public JSONObject put(final String api, Map<String, Object> params) {
-		return GrowthPush.getInstance().getHttpClient().put("1/" + api, params);
-	}
 
 }
