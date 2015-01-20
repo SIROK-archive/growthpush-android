740d90da5592196f71ccc3f674d0868b95825dab
Shigeru Ogawa <ogawa@sirok.co.jp>
2015-01-20 11:13:41 +0900

    Add no notificatition where run active process self app.

--- source/src/com/growthpush/handler/BaseReceiveHandler.java
+++ source/src/com/growthpush/handler/BaseReceiveHandler.java
@@ -1,13 +1,21 @@
 package com.growthpush.handler;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import android.app.ActivityManager;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
 
@@ -33,7 +41,7 @@ public class BaseReceiveHandler implements ReceiveHandler {
 
 	protected void showAlert(Context context, Intent intent) {
 
-		if (context == null || intent == null || intent.getExtras() == null)
+		if (context == null || intent == null || intent.getExtras() == null || checkFrontSelfApplication(context))
 			return;
 
 		Intent alertIntent = new Intent(context, AlertActivity.class);
@@ -48,11 +56,42 @@ public class BaseReceiveHandler implements ReceiveHandler {
 
 	protected void addNotification(Context context, Intent intent) {
 
-		if (context == null || intent == null || intent.getExtras() == null)
+		if (context == null || intent == null || intent.getExtras() == null || checkFrontSelfApplication(context))
 			return;
 
 		NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
 		notificationManager.notify("GrowthPush" + context.getPackageName(), 1, generateNotification(context, intent.getExtras()));
+
+	}
+
+	@SuppressWarnings("deprecation")
+	private boolean checkFrontSelfApplication(Context context) {
+
+		ActivityManager activityManager = (ActivityManager) context.getSystemService(Service.ACTIVITY_SERVICE);
+		String[] packages = new String[1];
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT_WATCH) {
+			final Set<String> activePackages = new HashSet<String>();
+			final List<ActivityManager.RunningAppProcessInfo> processInfos = activityManager.getRunningAppProcesses();
+			for (ActivityManager.RunningAppProcessInfo processInfo : processInfos) {
+				if (processInfo.importance == ActivityManager.RunningAppProcessInfo.IMPORTANCE_FOREGROUND) {
+					activePackages.addAll(Arrays.asList(processInfo.pkgList));
+				}
+			}
+			packages = activePackages.toArray(new String[activePackages.size()]);
+		} else {
+			ActivityManager.RunningTaskInfo taskInfo = activityManager.getRunningTasks(1).get(0);
+			packages[0] = taskInfo.topActivity.getPackageName();
+		}
+
+		if (packages == null || packages.length <= 0)
+			return false;
+
+		for (String packageName : packages) {
+			if (context.getPackageName().equals(packageName))
+				return true;
+		}
+
+		return false;
 
 	}
 
